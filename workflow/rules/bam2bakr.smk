0aec3df837d80c19f8f2be8bb50c0a9e02e1015c
iwv2 <isaac.vock@yale.edu>
2023-02-28 05:18:09 -0500

    Testing moving to bcftools mpileup for snp calling

--- workflow/rules/bam2bakr.smk
+++ workflow/rules/bam2bakr.smk
@@ -51,6 +51,7 @@ rule call_snps:
         nsamps = nctl
     output:
         "results/snps/snp.txt",
+        "results/snps/snp.vcf",
         temp("results/snps/mkdir.txt")
     log:
         "logs/call_snps/ctl_samps.log"
--- workflow/scripts/call_snps.sh
+++ workflow/scripts/call_snps.sh
@@ -16,21 +16,26 @@ unset control_samples[4]
 cpus=$1
 nsamps=$2
 output_txt=$3
-output_dir=$4
-genome_fasta=$5
+output_vcf=$4
+output_dir=$5
+genome_fasta=$6
 
 
 touch "$output_dir"
 
 
 
-if [ $nsamps > 0 ]; then
+if [ $nsamps > 0 ] 
+then
     # Loop through control samples:
         for cs in ${control_samples[@]}; do
             name=$(echo "$cs" | cut -d '/' -f 3 | rev | cut -c8- | rev)
             samtools sort -@ "$cpus" -o ./results/snps/"$name"_sort.bam "$cs"
             samtools index -@ "$cpus" ./results/snps/"$name"_sort.bam
+            bcftools mpileup --threads "$cpus" -f $genome_fasta "$cs"_sort.bam | bcftools call --threads "$cpus" -mv > ./results/snps/snp-"$cs".vcf
         done
+
+        cat ./results/snps/*.vcf > $output_vcf
 
         for cs in ${control_samples[@]}; do
             NAMES+=($(echo "$cs" | cut -d '/' -f 3 | rev | cut -c8- | rev))
@@ -41,12 +46,12 @@ if [ $nsamps > 0 ]; then
         # {1} : path to fasta file
         # {2} : samtools view -H ${control_samples[0]}_sort.bam | awk ' $1 == "@SQ" {split($2,a,":"); print a[2]}' : Extracts chromosome names from .bam file header
         # {3} : ${control_samples[@]/%/_sort.bam}                                                    : Appends "_sort.bam" to the end of control names and prints
-        parallel -j "$cpus" "samtools mpileup -uf {1} \
-                                                   -r {2} {3} \
-                                | bcftools call -mv" ::: $genome_fasta \
-                                                      ::: $(samtools view -H ./results/snps/${NAMES[0]}_sort.bam \
-                                                                    | awk ' $1 == "@SQ" {split($2,a,":"); print a[2]}') \
-                                                      ::: ./results/snps/${NAMES[@]/%/_sort.bam} > snp.vcf
+        #parallel -j "$cpus" "samtools mpileup -uf {1} \
+        #                                           -r {2} {3} \
+        #                        | bcftools call -mv" ::: $genome_fasta \
+        #                                              ::: $(samtools view -H ./results/snps/${NAMES[0]}_sort.bam \
+        #                                                            | awk ' $1 == "@SQ" {split($2,a,":"); print a[2]}') \
+        #                                              ::: ./results/snps/${NAMES[@]/%/_sort.bam} > snp.vcf
 
 
         # Note: Easier and also fast option would be:  bcftools mpileup --threads $cpus -f $genome_fasta "$cs"_sort.bam | bcftools call --threads $cpus-mv > snp-"$cs".vcf
@@ -58,11 +63,14 @@ if [ $nsamps > 0 ]; then
                                             else if (length($5) == 3 && $5 ~ /,/) {split($5, mut, ",")
                                                                                    print $4":"mut[1]":"$1":"$2
                                                                                    print $4":"mut[2]":"$1":"$2}
-                                            }' snp.vcf \
+                                            }' $output_vcf \
             | sort \
             | uniq > $output_txt
 
         mv snp.vcf ./results/snps/
 
         echo '* SNPs called and snp.txt generated'
+else
+    touch "$output_txt"
+    touch "$output_vcf"
 fi
