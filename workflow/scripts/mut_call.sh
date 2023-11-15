865422f339a89c80ab4f3bd03362bfd32c86c887
isaacvock <isaac.vock@gmail.com>
2023-11-15 13:10:22 -0500

    Added framework for RAM optimization

--- workflow/scripts/mut_call.sh
+++ workflow/scripts/mut_call.sh
@@ -131,6 +131,50 @@ fragment_size=$(echo "scale=0; $num_reads/$cpus" | bc)
 
     # 3) _cU.csv files
     if [ "$mut_pos" = "True" ]; then
+
+        # mkdir $dir/$sample
+
+        # if [ "$gzipped" = "TRUE" ]; then
+            
+        #     pigz -d -c $input_file > $dir/$sample/input_decompressed.csv
+
+        #     # Extract header
+        #     header=$(head -n 1 $dir/$sample/input_decompressed.csv)
+        #     tail -n +2 $dir/$sample/input_decompressed.csv > $dir/$sample/no_header.csv
+
+        #     rm -f $dir/$sample/input_decompressed.csv
+
+
+        # else
+            
+        #     # Extract header
+        #     header=$(head -n 1 $input_file)
+        #     tail -n +2 $input_file > $dir/$sample/no_header.csv
+
+
+        # fi
+
+
+        # # Stage 1: Split large CSV (without header) into smaller files
+        # split -l $lines $dir/$sample/no_header.csv $dir/$sample/chunk_ # Adjust the number of lines (10000) as needed
+
+        # # Stage 2: Sort each smaller file
+        # for file in $dir/$sample/chunk_*
+        # do
+        #     sort -t, -k$sort_column -V -o $file $file # Adjust delimiter (-t) and sort column (-k) as needed
+        # done
+
+        # # Stage 3: Merge sorted files into a single sorted file (without header)
+        # sort -m -t, -k$sort_column -V -o $dir/$sample/sorted_no_header.csv $dir/$sample/chunk_*
+
+        # # Add header back to the sorted file
+        # echo $header > $output_file
+        # cat $dir/$sample/sorted_no_header.csv >> $output_file
+
+        # # Clean up temporary files
+        # rm -r $dir/$sample/
+
+
         # Pre-sort cU fragments
         parallel -j $cpus "tail -n +2 {1} \
                                 | LC_COLLATE=C sort > {1.}_sort.csv" ::: ./results/counts/*_"$sample"_frag_cU.csv
